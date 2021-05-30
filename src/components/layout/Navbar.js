581b81b9c5515eafb6afbea320430e77c0850023
Jason Jones <jason.e.jones@gmail.com>
2021-05-30 09:39:58 -0400

    end of lecture 21 - basic setup of react router to a static About page while preserving state

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
   return (
@@ -7,6 +8,23 @@ const Navbar = ({ icon, title }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
+
+      {/* 
+      you could simple use a tags with href, but if you do you loose state.
+      For example - if you comkplete a search, then navigate to a differnt Link
+      when you navigate back to "home" you will have lost state and would
+      need to re-run the search.  But using react Link tag & to instead of
+      a tag & href we're able to preserve state
+      */}
+
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
