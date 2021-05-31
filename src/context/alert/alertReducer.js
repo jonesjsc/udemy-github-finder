224c0b62bb4997e9c61b1bb0c4fb16dd190bfe3b
Jason Jones <jason.e.jones@gmail.com>
2021-05-31 12:17:15 -0400

    end of lecture 33 - alert is a standalone component, and it's using context now too

---
+++ src/context/alert/alertReducer.js
@@ -0,0 +1,15 @@
+import { SET_ALERT, REMOVE_ALERT } from "../types";
+
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+
+export default (state, action) => {
+  switch (action.type) {
+    case SET_ALERT:
+      return action.payload;
+    case REMOVE_ALERT:
+      return null;
+
+    default:
+      return state;
+  }
+};
